da2b34ad6b16e426b1bde74b69f42fc6ec685f8d
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-01-19 15:56:52 +0300

    config UI, create layout

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Macbook on 19.01.22.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
