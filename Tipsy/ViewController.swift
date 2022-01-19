30040c2162bb1995620b59c287bbd12c62df09a7
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-01-19 12:07:31 +0300

    add files

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Macbook on 19.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
